061bc7998ec81b3671e92b8144fb4f6e82cfa11b
flore <flore@LAPTOP-92DN0AH1>
2025-02-12 18:36:16 -0800

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Interfas_grafica_dinamica
+{
+    public partial class Form1 : Form
+    {
+        private FlowLayoutPanel flowLayoutPanel;
+        private TextBox textBox;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtRuta.Text))
+            {
+                string ruta=txtRuta.Text;
+                flowLayoutPanel = new FlowLayoutPanel();
+                flowLayoutPanel.Dock = DockStyle.Fill;//dok para que utilise un espacio especifico y fill para que sea todo el espacio
+
+
+                //hacer invisibles los de inicio
+                btnIniciar.Visible = false;
+                txtRuta.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("no se a especificado una ruta");
+            }
+
+        }
+    }
+}
