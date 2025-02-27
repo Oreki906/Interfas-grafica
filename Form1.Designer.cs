5e1a539cf3d6691f4361b836cfe1b89b37bf175b
flore <flore@LAPTOP-92DN0AH1>
2025-02-26 18:57:14 -0800

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace prueba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.real = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // real
+            // 
+            this.real.Location = new System.Drawing.Point(63, 344);
+            this.real.Name = "real";
+            this.real.Size = new System.Drawing.Size(138, 22);
+            this.real.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 310);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "cadena de caracteres";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.real);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox real;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alejandro;
+
+namespace prueba2
+{
+    public partial class Form1 : Form
+    {
+        private TextBox txtSolonumeros;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Button boton = bipolar.hacerboton("clic",50, 150, Boton_Click);
+            Controls.Add(boton);
+
+            txtSolonumeros = bipolar.generarTexBox(50, 50, bipolar.TipoDato.Ambos);//cargar elexion de caracters letras, numeros o ambos
+            Controls.Add(txtSolonumeros);
+
+            
+            
+        }
+        
+
+        public void Boton_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("¡Botón clickeado desde DLL!");
+         string rfc = txtSolonumeros.Text;
+            bool esValido =Verificar.RFCvalido(rfc);
+
+
+         string cadena = real.Text;
+            bool esvalido = Verificar.cadenanumeros(cadena);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+    
+}
